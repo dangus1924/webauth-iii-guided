e89b2193052eb750f9a85fe6d09f0853d3705db2
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-25 08:32:49 -0500

    installed packages

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,30 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorzation;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  //see if ther is a token
+  //  check if it valid => rehash the header + payload + secret and see if it matches oiur verify signature
+  // user id or username
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({
+          message: 'not verified'
+        })
+      } else {
+        req.decodedToken = decodedToken
+        next();
+      }
+    });
+
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({
+      message: 'no token provided'
+    })
   }
 };
